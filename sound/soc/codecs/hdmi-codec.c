6c51b591234f23b63c5509cfab5cd26b0e93fb85
orangepi-xunlong <258384131@qq.com>
2021-11-26 16:10:21 +0800

    Support h616 ahub and hdmi audio

--- sound/soc/codecs/hdmi-codec.c
+++ sound/soc/codecs/hdmi-codec.c
@@ -435,6 +435,7 @@ static int hdmi_codec_startup(struct snd_pcm_substream *substream,
 	if (!((has_playback && tx) || (has_capture && !tx)))
 		return 0;
 
+#if ! defined(CONFIG_SND_SOC_SUN50I_HDMI)
 	mutex_lock(&hcp->lock);
 	if (hcp->busy) {
 		dev_err(dai->dev, "Only one simultaneous stream supported!\n");
@@ -447,6 +448,7 @@ static int hdmi_codec_startup(struct snd_pcm_substream *substream,
 		if (ret)
 			goto err;
 	}
+#endif
 
 	if (tx && hcp->hcd.ops->get_eld) {
 		ret = hcp->hcd.ops->get_eld(dai->dev->parent, hcp->hcd.data,
@@ -480,12 +482,14 @@ static void hdmi_codec_shutdown(struct snd_pcm_substream *substream,
 	if (!((has_playback && tx) || (has_capture && !tx)))
 		return;
 
+#if ! defined(CONFIG_SND_SOC_SUN50I_HDMI)
 	hcp->chmap_idx = HDMI_CODEC_CHMAP_IDX_UNKNOWN;
 	hcp->hcd.ops->audio_shutdown(dai->dev->parent, hcp->hcd.data);
 
 	mutex_lock(&hcp->lock);
 	hcp->busy = false;
 	mutex_unlock(&hcp->lock);
+#endif
 }
 
 static int hdmi_codec_fill_codec_params(struct snd_soc_dai *dai,
@@ -1055,6 +1059,15 @@ static int hdmi_codec_probe(struct platform_device *pdev)
 			__func__, ret);
 		return ret;
 	}
+
+#if defined(CONFIG_SND_SOC_SUN50I_HDMI)
+        if (hcp->hcd.ops->audio_startup) {
+                ret = hcp->hcd.ops->audio_startup(dev, hcp->hcd.data);
+                if (ret)
+			return 0;
+        }
+#endif
+
 	return 0;
 }
 
